14897e92e88850330f4dba27a932f9107970a941
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2024-01-10 13:42:32 -0700

    01 - Login Desing

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import LoginForm from '@/components/LoginForm'
+
 
 export default function Home() {
   return (
     <main >
-     <h1>Hellow World</h1>
+     <LoginForm />
     </main>
   )
 }
