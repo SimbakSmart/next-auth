9722f352047245debe64948f0f4a4ee05bf58105
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2024-01-10 13:48:06 -0700

    02 - Register Design

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,7 +1,8 @@
+import RegisterForm from '@/components/RegisterForm'
 import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register Page</div>
+    <RegisterForm />
   )
 }
